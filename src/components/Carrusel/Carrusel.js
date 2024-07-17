1ece512d2de53c51ad5ec78a92f5e2f296853c90
Matías Durand <141950969+MatiasD1@users.noreply.github.com>
2024-07-17 18:04:23 -0300

    Agregando texto a cada imagen del carrusel

--- src/components/Carrusel/Carrusel.js
+++ src/components/Carrusel/Carrusel.js
@@ -1,10 +1,10 @@
-import 'bootstrap/dist/js/bootstrap.bundle.min.js'; // Había que importarlo para que funcione el carrusel
+import React from 'react';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js'; // Importar Bootstrap JS para el carrusel
 import fotoPieza from "../../img/fotoPieza.jpg";
 import fotoExterior from "../../img/fotoExterior.jpg";
 import fotoSala from "../../img/fotoSala.jpg";
 import flechaDer from "../../img/icons/flechaDer.png";
 import flechaIzq from "../../img/icons/flechaIzq.png";
-
 
 const Carrusel = () => {
     return (
@@ -12,21 +12,24 @@ const Carrusel = () => {
             <div id="carouselExampleAutoplaying" className="carousel slide" data-bs-ride="carousel" data-bs-interval="3000">
                 <div className="carousel-inner carrusel">
                     <div className="carousel-item active">
-                        <img src={fotoPieza} className="d-block w-100" alt="Foto de la pieza"/>
+                        <img src={fotoPieza} className="d-block w-100" alt="Foto de la pieza" />
+                        <p className='textoCarruselInicio'>Texto para la primera imagen</p>
                     </div>
                     <div className="carousel-item">
-                        <img src={fotoExterior} className="d-block w-100" alt="Foto del exterior"/>
+                        <img src={fotoExterior} className="d-block w-100" alt="Foto del exterior" />
+                        <p className='textoCarruselInicio'>Texto para la segunda imagen</p>
                     </div>
                     <div className="carousel-item">
-                        <img src={fotoSala} className="d-block w-100" alt="Foto de la sala"/>
+                        <img src={fotoSala} className="d-block w-100" alt="Foto de la sala" />
+                        <p className='textoCarruselInicio'>Texto para la tercera imagen</p>
                     </div>
                 </div>
                 <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="prev">
-                    <img src={flechaIzq} alt="Anterior" className="flecha"/>
+                    <img src={flechaIzq} alt="Anterior" className="flecha" />
                     <span className="visually-hidden">Previous</span>
                 </button>
                 <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="next">
-                    <img src={flechaDer} alt="Siguiente" className="flecha"/>
+                    <img src={flechaDer} alt="Siguiente" className="flecha" />
                     <span className="visually-hidden">Next</span>
                 </button>
             </div>
