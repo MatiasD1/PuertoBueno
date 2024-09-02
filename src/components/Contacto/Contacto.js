ee6f27f66bf9b1e2e566c5d1ad404d996dfd42f1
Matías Durand <141950969+MatiasD1@users.noreply.github.com>
2024-09-02 16:31:01 -0300

    Cambio de formulario y paleta de colores

--- src/components/Contacto/Contacto.js
+++ src/components/Contacto/Contacto.js
@@ -20,18 +20,13 @@ const Contacto = () => {
 
     const handleSubmit = (e) => {
         e.preventDefault();
-
         fetch('http://localhost:5000/send', {
             method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
+            headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify(formData)
         })
         .then(response => response.text())
-        .then(data => {
-            alert(data);
-        })
+        .then(data => alert(data))
         .catch(error => {
             console.error('Error:', error);
             alert('Error al enviar el formulario');
@@ -40,42 +35,26 @@ const Contacto = () => {
 
     return (
         <div className="contacto">
-            
             <div className="contact-form">
                 <h2>Contacto</h2>
                 <form onSubmit={handleSubmit}>
-                    <label htmlFor="nombre">Nombre Completo:</label>
-                    <input type="text" id="nombre" name="nombre" value={formData.nombre} onChange={handleChange} required />
+                    <input type="text" placeholder="Nombre Completo" name="nombre" value={formData.nombre} onChange={handleChange} required />
                     <div className="fechasContacto">
-                        <div className="fecha1">
-                            <label className="fechaEntrada">Fecha de Entrada:</label>
-                            <input type="date" id="fechaEntrada" name="fechaEntrada" value={formData.fechaEntrada} onChange={handleChange} required />
-                        </div>                          
-                        <div className="fecha2">
-                            <label className="fechaSalida">Fecha de Salida:</label>
-                            <input type="date" id="fechaSalida" name="fechaSalida" value={formData.fechaSalida} onChange={handleChange} required />       
-                        </div>
+                        <input type="date" name="fechaEntrada" value={formData.fechaEntrada} onChange={handleChange} required />
+                        <input type="date" name="fechaSalida" value={formData.fechaSalida} onChange={handleChange} required />
                     </div>
-                    
-                    <label htmlFor="cantidadPersonas">Cantidad de Personas:</label>
-                    <input type="number" id="cantidadPersonas" name="cantidadPersonas" value={formData.cantidadPersonas} onChange={handleChange} required />
-
-                    <label htmlFor="email">Email:</label>
-                    <input type="email" id="email" name="email" value={formData.email} onChange={handleChange} required />
-
-                    <label htmlFor="mensaje">Mensaje:</label>
-                    <textarea id="mensaje" name="mensaje" value={formData.mensaje} onChange={handleChange}></textarea>
-
+                    <input type="number" placeholder="Cantidad de Personas" name="cantidadPersonas" value={formData.cantidadPersonas} onChange={handleChange} required />
+                    <input type="email" placeholder="Email" name="email" value={formData.email} onChange={handleChange} required />
+                    <textarea placeholder="Mensaje" name="mensaje" value={formData.mensaje} onChange={handleChange}></textarea>
                     <button type="submit">Enviar</button>
                 </form>
-            </div>  
+            </div>
             <div className="textoContacto">
                 <h3>
-                    El tiempo de vacaciones suele ser poco,  idealmente, su calidad debe ser mucha. 
-                    Por lo tanto, para nosotros,  una reserva es sinónimo de responsabilidad y compromiso.<br/><br/> 
-                    Podés contar con la seguridad que da el trato directo con los propietarios de este proyecto.<br/><br/> 
-                    Queremos que te sientas tan a gusto, que nos recomiendes y nos vuelvas a elegir cada vez que
-                    visites MdP.<br/>
+                    El tiempo de vacaciones suele ser poco, idealmente, su calidad debe ser mucha.
+                    Por lo tanto, para nosotros, una reserva es sinónimo de responsabilidad y compromiso.<br/><br/>
+                    Podés contar con la seguridad que da el trato directo con los propietarios de este proyecto.<br/><br/>
+                    Queremos que te sientas tan a gusto, que nos recomiendes y nos vuelvas a elegir cada vez que visites MdP.
                 </h3>
             </div>
         </div>
